0157ce55b39512de9863508d3cae012f16ab323a
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-04-02 17:18:06 +0430

    update port number

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,6 +1,6 @@
 import { dev } from '$app/env';
 
-export const baseUrl = dev ? 'http://localhost:3000' : 'https://minibase.deno.dev';
+export const baseUrl = dev ? 'http://localhost:8000' : 'https://minibase.deno.dev';
 
 export function generateApiKey() {
 	const result = Array.from({ length: 32 }, () => {
