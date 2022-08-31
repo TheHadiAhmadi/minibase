b37cc1e82e8166512d4d44d9cb071ca1c4be85b8
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-08-31 23:37:56 +0430

    updaate

--- svelte.config.js
+++ svelte.config.js
@@ -20,35 +20,3 @@ export default {
 		})
 	}
 }
-
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	// Consult https://github.com/sveltejs/svelte-preprocess
-// 	// for more information about preprocessors
-// 	preprocess: preprocess(),
-
-// 	kit: {
-// 		// adapter: adapter(),
-// 		adapter: adapter({
-// 			serverFile: path.resolve('output/server.js'),
-// 			out: './output/build',
-// 			filesPrefix: './build',
-// 			imports: {
-// 				jsonwebtoken: 'https://jspm.dev/jsonwebtoken@8.5.1',
-// 				crypto: 'https://deno.land/std/node/crypto.ts',
-// 				nanoid: 'https://deno.land/x/nanoid/mod.ts'
-// 			}
-// 		}),
-// 		vite: {
-// 			plugins: [windicss()],
-// 			test: {
-// 				globals: true,
-// 				environment: 'jsdom',
-// 				includeSource: ['src/**/*.{js,ts,svelte}']
-// 			}
-// 		}
-// 	}
-// };
-
-// export default config;
