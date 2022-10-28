a23e3fa96f2766f8a1809319c9332ea7f7e673b6
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-10-28 22:13:09 +0430

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,25 @@
-import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
-import path from 'path'
+import adapter from "@sveltejs/adapter-vercel";
+import preprocess from "svelte-preprocess";
+import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
-		alias: {
-			'$services': path.resolve('./src/services'),
-			'$components': path.resolve('./src/components'),
-			'$types': path.resolve('./src/types'),
-			'$stores': path.resolve('./src/stores'),
-			'$server': path.resolve('./src/server'),
-		}
-	}
+  kit: {
+    adapter: adapter({
+      edge: true,
+    }),
+    alias: {
+      $services: path.resolve("./src/services"),
+      $components: path.resolve("./src/components"),
+      $types: path.resolve("./src/types"),
+      $stores: path.resolve("./src/stores"),
+      $server: path.resolve("./src/server"),
+    },
+  },
 };
 
 export default config;
