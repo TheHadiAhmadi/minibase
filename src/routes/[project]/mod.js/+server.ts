1d3acfc51c8bd27f84c22b1c3459b1102745f13f
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-12-09 17:06:57 +0430

    update backend url

--- src/routes/[project]/mod.js/+server.ts
+++ src/routes/[project]/mod.js/+server.ts
@@ -1,9 +1,9 @@
 import type { RequestEvent } from "./$types";
 
 export async function GET({ params }: RequestEvent) {
-  const code = await fetch(`https://cloud-3.domcloud.io/${params.project}/mod.js`).then(
-    (res) => res.text()
-  );
+  const code = await fetch(
+    `https://minibase-api.onrender.com/${params.project}/mod.js`
+  ).then((res) => res.text());
 
   return new Response(code, {
     headers: { "Content-Type": "text/javascript" },
